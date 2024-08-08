51bd4f428c9087d5e8f37dcaf4fc8e8111c854ef
georgesamoskey <georgeskatiera@gmail.com>
2024-08-08 11:04:29 +0200

    Le

--- src/main/java/com/payall/gateway/GatewayApplication.java
+++ src/main/java/com/payall/gateway/GatewayApplication.java
@@ -10,7 +10,7 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-		System.out.println("DERNIER COMMIT");
+		System.out.println("DERNIER COMMIT SANS");
 	}
 
 }
