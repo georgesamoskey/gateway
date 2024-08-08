4bdd5b973b70df73825258f7943c6ef43801059f
georgesamoskey <georgeskatiera@gmail.com>
2024-08-08 14:40:30 +0200

    Le dernier commit

--- src/main/java/com/payall/gateway/GatewayApplication.java
+++ src/main/java/com/payall/gateway/GatewayApplication.java
@@ -10,7 +10,6 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-		System.out.println("DERNIER COMMIT SANS");
 	}
 
 }
