66eabcceb9644717ab082a39fb195eb0b8990386
georgesamoskey <georgeskatiera@gmail.com>
2024-08-26 14:37:49 +0200

    g

--- src/main/java/com/payall/gateway/GatewayApplication.java
+++ src/main/java/com/payall/gateway/GatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		System.out.println("data check up");
+		System.out.println("data check up  bingo");
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
